9693aac344ace069ad8c2651b5ec845a46263690
see-bet-code <brittani.elan.taylor@gmail.com>
2020-09-17 12:18:13 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appt|
+            appt.doctor == self
+        end
+    end
+
+    def new_appointment(patient, date)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        self.appointments.map { |appt| appt.patient }
+    end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,22 @@
+class Genre
+    attr_accessor :name
+    @@all = []
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def artists
+        self.songs.map { |song| song.artist }
+    end
+end
